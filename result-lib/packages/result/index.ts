d3afaa4f425c57c55c2aa915390970ff316647cb
Ihor Vodianyi <vodyanoy_igor@hotmail.com>
2023-08-04 10:02:14 +0200

    Commit

--- result-lib/packages/result/index.ts
+++ result-lib/packages/result/index.ts
@@ -3,7 +3,7 @@ export enum ResultState {
     OK
 }
 
-abstract class TResultBase {
+export abstract class ResultBase {
 
     private _state: ResultState;
 
@@ -11,42 +11,46 @@ abstract class TResultBase {
         this._state = state;
     }
 
-    public isFailed(): boolean {
+    public get isFailed(): boolean {
         return this._state == ResultState.FAIL;
     }
-    public isSuccess(): boolean {
+    public get isSuccess(): boolean {
         return this._state == ResultState.OK;
     }
 }
 
-export class TResult extends TResultBase {
+export class Result extends ResultBase {
 
-    public static ok(): TResult {
-        return new TResult(ResultState.OK);
+    private constructor(state: ResultState) {
+        super(state);
     }
 
-    public static fail(): TResult {
-        return new TResult(ResultState.FAIL);
+    public static ok(): Result {
+        return new Result(ResultState.OK);
+    }
+
+    public static fail(): Result {
+        return new Result(ResultState.FAIL);
     }
 }
 
-export class TResultValue<TValue> extends TResultBase {
+export class ResultValue<TValue> extends ResultBase {
 
     private _value: TValue | undefined;
     public get value(): TValue | undefined {
         return this._value;
     }
 
-    constructor(state: ResultState, value: TValue | undefined) {
+    private constructor(state: ResultState, value: TValue | undefined) {
         super(state);
         this._value = value;
     }
 
-    public static ok<TValue>(value: TValue): TResultValue<TValue> {
-        return new TResultValue<TValue>(ResultState.OK, value);
+    public static ok<TValue>(value: TValue): ResultValue<TValue> {
+        return new ResultValue<TValue>(ResultState.OK, value);
     }
 
-    public static fail<TValue>(): TResultValue<TValue> {
-        return new TResultValue<TValue>(ResultState.FAIL, undefined);
+    public static fail<TValue>(): ResultValue<TValue> {
+        return new ResultValue<TValue>(ResultState.FAIL, undefined);
     }
 }
